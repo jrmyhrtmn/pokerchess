c96842d657923de166b6e4d4143e375412ec72c9
Jeremy Hartman <jeremy@jrmyhrtmn.com>
2019-04-26 07:05:33 -0400

    Added assets

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './components.css';
+import pawn from '../icons/pawn.svg';
+import rook from '../icons/rook.svg';
+import bishop from '../icons/bishop.svg';
+import knight from '../icons/knight.svg';
+import king from '../icons/king.svg';
+import queen from '../icons/queen.svg';
+import cardBack from '../icons/card-back.svg';
 
 export default function Card(props) {
+  let pieces = [pawn, rook, bishop, knight, king, queen];
   return (
     <button className={"card"}
             onClick={props.onClick}>
-      {props.piece}
+            <img src={(props.piece == -1) ? cardBack : pieces[props.piece]} />
     </button>
   );
 }
--- src/components/square.js
+++ src/components/square.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './components.css';
+import player from '../icons/player.svg';
+import opponent from '../icons/opponent.svg';
 
 export default function Square(props) {
+  let icon = (props.player == 1) ? player : opponent;
   return (
     <button className={"square " + props.color} onClick={props.onClick}>
-      {props.player}
+      {props.player ? <img src={icon} /> : null}
     </button>
   );
 }
